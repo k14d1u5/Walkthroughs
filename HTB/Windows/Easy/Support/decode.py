6858ef1d00f0ce97ff071934c3d20027aef668ea
Claudio <46407674+k14d1u5@users.noreply.github.com>
2025-03-30 12:42:53 +0200

    Added decode.py script in Support folder

---
+++ HTB/Windows/Easy/Support/decode.py
@@ -0,0 +1,25 @@
+'''
+Author: k14d1u5
+Date: 11/12/2024
+Language: Python
+Description: Script to decode the password found in UserInfo.exe, useful to solve the Support HTB  box.
+
+'''
+
+import base64
+
+encpassword = "0Nv32PTwgYjzg9/8j5TbmvPd3e7WhtWWyuPsyO76/Y+U193E"
+key = b"armando"
+
+# Decodifica Base64
+array = base64.b64decode(encpassword)
+
+# Applica XOR per ogni byte
+array2 = bytearray()
+for i in range(len(array)):
+    array2.append(array[i] ^ key[i % len(key)] ^ 0xDF)
+
+# Restituisce la stringa decodificata
+result = array2.decode("latin1")  # latin1 per compatibilità con Encoding.Default di C#
+
+print(result)
